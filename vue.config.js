cd03cf7f535b83a6201de3bcaa2f9c0411a2188e
Plamen Ivanov <plamen.ivanov@acronis.com>
2021-05-25 16:14:40 +0300

    Added most of the needed npm packages and removed unused stuff

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	//change the app/html title
+	chainWebpack: (config) => {
+		config
+			.plugin('html')
+			.tap((args) => {
+				args[0].title = 'GameHub';
+				return args;
+			});
+	},
+	css: {
+		loaderOptions: {
+			sass: {
+				//imports the provided scss files globally (in every vue component)
+				//so you don't have to import them manually
+				prependData: `
+					@import '@/assets/css/_variables.scss';
+				`
+			}
+		}
+	}
+};
