191364446a04a6e1250a766eaaccf40f76c651e1
Plamen Ivanov <plamen.ivanov@acronis.com>
2021-10-20 20:41:25 +0300

    Refactored the pong collision manager

--- games/pong/misc/collisions-manager.js
+++ games/pong/misc/collisions-manager.js
@@ -31,23 +31,23 @@ export default class CollisionsManager {
 			const player = paddle.player;
 
 			//top end of screen
-			if (paddle.y < 0) {
-				paddle.y = 0;
+			if (paddle.top < 0) {
+				paddle.top = 0;
 				paddle.dy = 0;
 			}
 
 			//bottom end of screen
-			if (paddle.y + paddle.height > paddle.canvas.height) {
-				paddle.y = paddle.canvas.height - paddle.height;
+			if (paddle.bottom > paddle.canvas.height) {
+				paddle.bottom = paddle.canvas.height;
 				paddle.dy = 0;
 			}
 
 			//paddle collides with ball
 			if (Utils.collidesWith(paddle.hitbox, ball.hitbox)) {
 				if (player === 1) {
-					ball.x = paddle.x + paddle.width;
+					ball.left = paddle.right;
 				} else {
-					ball.x = paddle.x - ball.width;
+					ball.right = paddle.left;
 				}
 
 				ball.dx = ball.dx * -1;
@@ -65,20 +65,20 @@ export default class CollisionsManager {
 		const ball = this.game.ball;
 
 		//top end of screen
-		if (ball.y < 0) {
-			ball.y = 0;
+		if (ball.top < 0) {
+			ball.top = 0;
 			ball.dy = ball.dy * -1;
 		}
 
 		//bottom end of scren
-		if (ball.y + ball.height > ball.canvas.height) {
-			ball.y = ball.canvas.height - ball.height;
+		if (ball.bottom > ball.canvas.height) {
+			ball.bottom = ball.canvas.height;
 			ball.dy = ball.dy * -1;
 		}
 
 		//left end of screen
-		if (ball.x < 0) {
-			ball.x = 0;
+		if (ball.left < 0) {
+			ball.left = 0;
 			ball.dx = ball.dx * -1;
 
 			if (this.game.isServer) {
@@ -87,8 +87,8 @@ export default class CollisionsManager {
 		}
 
 		//right end of screen
-		if (ball.x + ball.width > ball.canvas.width) {
-			ball.x = ball.canvas.width - ball.width;
+		if (ball.right > ball.canvas.width) {
+			ball.right = ball.canvas.width;
 			ball.dx = ball.dx * -1;
 
 			if (this.game.isServer) {
